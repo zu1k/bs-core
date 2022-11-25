4afb86b0315dfb521f112532e36d855bfa089d6f
zu1k <i@zu1k.com>
2022-11-25 14:48:07 +0800

    chore: add build script

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash	
+
+NAME=book-searcher
+
+targets=(x86_64-unknown-linux-gnu x86_64-unknown-linux-musl)
+targets_win=(x86_64-pc-windows-gnu)
+
+for target in "${targets[@]}"
+do
+    echo $target
+    cargo build --release --target $target
+    pushd target/$target/release/ && zip book-searcher-$target.zip $NAME && mv book-searcher-$target.zip ../../ && popd
+done
+
+for target in "${targets_win[@]}"
+do
+    echo $target
+    cargo build --release --target $target
+    pushd target/$target/release/ && zip book-searcher-$target.zip $NAME.exe && mv book-searcher-$target.zip ../../ && popd
+done
