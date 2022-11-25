10d815b91d3a8bf05802a187d27a9855909bd4d8
zu1k <i@zu1k.com>
2022-11-25 14:48:07 +0800

    chore: add build script

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash	
+
+NAME=zlib-searcher
+
+targets=(x86_64-unknown-linux-gnu x86_64-unknown-linux-musl)
+targets_win=(x86_64-pc-windows-gnu)
+
+for target in "${targets[@]}"
+do
+    echo $target
+    cargo build --release --target $target
+    pushd target/$target/release/ && zip zlib-searcher-$target.zip $NAME && mv zlib-searcher-$target.zip ../../ && popd
+done
+
+for target in "${targets_win[@]}"
+do
+    echo $target
+    cargo build --release --target $target
+    pushd target/$target/release/ && zip zlib-searcher-$target.zip $NAME.exe && mv zlib-searcher-$target.zip ../../ && popd
+done
