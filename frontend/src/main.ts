f508bb1fd84d1fd9cd7a222b561a9d80ae6b0796
zu1k <i@zu1k.com>
2022-11-30 10:34:58 +0800

    chore: merge frontend branch to master

---
+++ frontend/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+
+createApp(App).use(Antd).mount('#app')
