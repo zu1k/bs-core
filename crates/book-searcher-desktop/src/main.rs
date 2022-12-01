a304a63e389a5c93474574bab0a25920ec2ed405
zu1k <i@zu1k.com>
2022-12-01 10:33:41 +0800

    feat: init desktop

---
+++ crates/book-searcher-desktop/src/main.rs
@@ -0,0 +1,50 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::sync::Arc;
+use book_searcher_core::{Book, Searcher};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+struct AppState {
+    searcher: Arc<Searcher>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        let index_dir = std::env::current_exe()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("index")
+            .to_str()
+            .unwrap()
+            .to_string();
+        AppState {
+            searcher: Arc::new(Searcher::new(&index_dir)),
+        }
+    }
+}
+
+#[tauri::command]
+fn search(state: tauri::State<AppState>, query: String, limit: usize) -> Vec<Book> {
+    let searcher = state.searcher.clone();
+    searcher.search(&query, limit)
+}
+
+#[tauri::command]
+fn version() -> String {
+    VERSION.to_string()
+}
+
+fn main() {
+    let state = AppState::new();
+
+    tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![version, search])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
