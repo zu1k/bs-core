4696d3f8de09346d8009ec8c0858766b1c863060
zu1k <i@zu1k.com>
2023-01-01 19:15:15 +0800

    chore: add release ci

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -6,13 +6,31 @@ project=book-searcher
 targets=()
 features=()
 
-while getopts "t:f:u" opt; do
+ALL_TARGETS="
+i686-unknown-linux-musl
+x86_64-pc-windows-gnu
+x86_64-unknown-linux-gnu
+x86_64-unknown-linux-musl
+armv7-unknown-linux-musleabihf
+armv7-unknown-linux-gnueabihf
+arm-unknown-linux-gnueabi
+arm-unknown-linux-gnueabihf
+arm-unknown-linux-musleabi
+arm-unknown-linux-musleabihf
+aarch64-unknown-linux-gnu
+aarch64-unknown-linux-musl
+"
+
+while getopts "p:t:a:f" opt; do
     case $opt in
         p)
             project=($OPTARG)
             ;;
         t)
             targets+=($OPTARG)
+            ;;
+        a)
+            targets+=($ALL_TARGETS)
             ;;
         f)
             features+=($OPTARG)
@@ -44,11 +62,13 @@ function build() {
         cross build --target "${TARGET}" \
                     --default-features=false
                     --features "${TARGET_FEATURES}" \
+                    -p book-searcher \
                     --release
     else
         echo "* Building ${project} package for ${TARGET} ..."
 
         cross build --target "${TARGET}" \
+                    -p book-searcher \
                     --release
     fi
 
@@ -59,14 +79,13 @@ function build() {
     PKG_DIR="${CUR_DIR}/../release"
     mkdir -p "${PKG_DIR}"
 
-    if [[ "$TARGET" == *"-linux-"* ]]; then
-        PKG_NAME="${project}-${TARGET}.tar.gz"
+    if [[ "$TARGET" == *"-windows-"* ]]; then
+        PKG_NAME="${project}-${TARGET}.zip"
         PKG_PATH="${PKG_DIR}/${PKG_NAME}"
 
+        echo "* Packaging ZIP in ${PKG_PATH} ..."
         cd ${RELEASE_DIR}
-
-        echo "* Packaging gz in ${PKG_PATH} ..."
-        tar -czf ${PKG_PATH} ${project}
+        zip ${PKG_PATH} ${project}.exe
 
         if [[ $? != "0" ]]; then
             exit 1
@@ -74,13 +93,14 @@ function build() {
 
         cd "${PKG_DIR}"
         shasum -a 256 "${PKG_NAME}" > "${PKG_NAME}.sha256"
-    elif [[ "$TARGET" == *"-windows-"* ]]; then
-        PKG_NAME="${project}-${TARGET}.zip"
+    else
+        PKG_NAME="${project}-${TARGET}.tar.gz"
         PKG_PATH="${PKG_DIR}/${PKG_NAME}"
 
-        echo "* Packaging ZIP in ${PKG_PATH} ..."
         cd ${RELEASE_DIR}
-        zip ${PKG_PATH} ${project}.exe
+
+        echo "* Packaging gz in ${PKG_PATH} ..."
+        tar -czf ${PKG_PATH} ${project}
 
         if [[ $? != "0" ]]; then
             exit 1
