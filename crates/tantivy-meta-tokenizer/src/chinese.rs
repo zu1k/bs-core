6876d8c1df2d3bbea1b405180bf9ece65e17db81
zu1k <i@zu1k.com>
2023-01-06 22:48:04 +0800

    feat: T2S before jieba tokenize

--- crates/tantivy-meta-tokenizer/src/chinese.rs
+++ crates/tantivy-meta-tokenizer/src/chinese.rs
@@ -1,24 +1,31 @@
+use jieba_rs::{Jieba, TokenizeMode};
 use tantivy::tokenizer::{BoxTokenStream, Token};
 
 use crate::MetaTokenStream;
 
 lazy_static::lazy_static! {
-    static ref JIEBA: jieba_rs::Jieba = jieba_rs::Jieba::new();
+    static ref JIEBA: Jieba = jieba_rs::Jieba::new();
 }
 
 pub fn token_stream(text: &str) -> BoxTokenStream {
+    let text = fast2s::convert(text);
+
     let mut indices = text.char_indices().collect::<Vec<_>>();
     indices.push((text.len(), '\0'));
-    let orig_tokens = JIEBA.tokenize(text, jieba_rs::TokenizeMode::Search, false);
+
+    let origin_tokens = JIEBA.tokenize(&text, TokenizeMode::Search, false);
+    let origin_tokens_len = origin_tokens.len();
     let mut tokens = Vec::new();
-    for token in &orig_tokens {
+
+    for (token_idx, token) in origin_tokens.iter().enumerate() {
         tokens.push(Token {
             offset_from: indices[token.start].0,
             offset_to: indices[token.end].0,
-            position: token.start,
             text: String::from(&text[(indices[token.start].0)..(indices[token.end].0)]),
-            position_length: token.end - token.start,
+            position: token_idx + 1,
+            position_length: origin_tokens_len,
         });
     }
+
     BoxTokenStream::from(MetaTokenStream { tokens, index: 0 })
 }
