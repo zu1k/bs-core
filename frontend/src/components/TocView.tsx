5a1ffb3af7a7cf838337b354367ee56b0a9728f5
zu1k <i@zu1k.com>
2023-01-13 19:17:14 +0800

    fix: book preview toc scroll

--- frontend/src/components/TocView.tsx
+++ frontend/src/components/TocView.tsx
@@ -39,7 +39,7 @@ const TocView: React.FC<TocViewProps> = ({
           </Heading>
         </DrawerHeader>
 
-        <DrawerBody>
+        <DrawerBody overflowY="scroll">
           {toc.map((item, index) => (
             <Box
               fontSize="md"
