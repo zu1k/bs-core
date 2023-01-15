be0d9a5bb088a1431e3f6ed944ebde7110500b70
zu1k <i@zu1k.com>
2023-01-15 16:53:38 +0800

    fix: handle multi pinyin char

--- crates/tantivy-meta-tokenizer/src/chinese.rs
+++ crates/tantivy-meta-tokenizer/src/chinese.rs
@@ -14,20 +14,45 @@ pub fn token_stream(text: &str) -> BoxTokenStream {
 
     let origin_tokens = JIEBA.tokenize(&text, TokenizeMode::Search, false);
     let origin_tokens_len = origin_tokens.len();
-    let mut tokens = Vec::new();
+
+    #[cfg(not(feature = "pinyin"))]
+    let all_tokens_len = origin_tokens_len;
+
+    #[cfg(feature = "pinyin")]
+    use itertools::Itertools;
+    #[cfg(feature = "pinyin")]
+    use pinyin::ToPinyinMulti;
+    #[cfg(feature = "pinyin")]
+    let chars_pinyins: Vec<(usize, String)> = text
+        .as_str()
+        .to_pinyin_multi()
+        .enumerate()
+        .filter_map(|(idx, pm)| {
+            pm.map(|pm| {
+                pm.into_iter()
+                    .map(move |p| (idx, p.plain().to_string()))
+                    .unique()
+            })
+        })
+        .flatten()
+        .collect();
+    #[cfg(feature = "pinyin")]
+    let all_tokens_len = origin_tokens_len * 2 + chars_pinyins.len();
+
+    let mut tokens = Vec::with_capacity(all_tokens_len);
 
     for (token_idx, token) in origin_tokens.iter().enumerate() {
         tokens.push(Token {
             offset_from: indices[token.start].0,
             offset_to: indices[token.end].0,
-            text: String::from(&text[(indices[token.start].0)..(indices[token.end].0)]),
-            position: token_idx + 1,
-            position_length: origin_tokens_len,
+            text: token.word.to_string(),
+            position: token_idx * 2 + 1,
+            position_length: all_tokens_len,
         });
 
         #[cfg(feature = "pinyin")]
         {
-            // every token
+            // every token, not pinyin_multi to limit the index size
             use pinyin::ToPinyin;
             let pinyin: String = token
                 .word
@@ -40,8 +65,8 @@ pub fn token_stream(text: &str) -> BoxTokenStream {
                 offset_from: indices[token.start].0,
                 offset_to: indices[token.end].0,
                 text: pinyin,
-                position: token_idx + 1,
-                position_length: origin_tokens_len,
+                position: token_idx * 2 + 2,
+                position_length: all_tokens_len,
             });
         }
     }
@@ -49,21 +74,15 @@ pub fn token_stream(text: &str) -> BoxTokenStream {
     #[cfg(feature = "pinyin")]
     {
         // every Chinese char
-        use pinyin::ToPinyin;
-        let text_len = text.chars().count();
-        text.chars()
-            .filter_map(|c| c.to_pinyin())
-            .map(|p| p.plain())
-            .enumerate()
-            .for_each(|(i, p)| {
-                tokens.push(Token {
-                    offset_from: indices[i].0,
-                    offset_to: indices[i + 1].0,
-                    text: String::from(p),
-                    position: i + 1,
-                    position_length: text_len,
-                });
+        chars_pinyins.into_iter().for_each(|(idx, p)| {
+            tokens.push(Token {
+                offset_from: indices[idx].0,
+                offset_to: indices[idx + 1].0,
+                text: p,
+                position: origin_tokens_len * 2 + idx + 1,
+                position_length: all_tokens_len,
             });
+        });
     }
 
     BoxTokenStream::from(MetaTokenStream { tokens, index: 0 })
