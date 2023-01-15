2474a05d0293e864292472aacce24bf0c791daca
zu1k <i@zu1k.com>
2023-01-15 14:25:45 +0800

    feat: token to pinyin

--- crates/tantivy-meta-tokenizer/src/chinese.rs
+++ crates/tantivy-meta-tokenizer/src/chinese.rs
@@ -1,7 +1,6 @@
+use crate::MetaTokenStream;
 use jieba_rs::{Jieba, TokenizeMode};
 use tantivy::tokenizer::{BoxTokenStream, Token};
-
-use crate::MetaTokenStream;
 
 lazy_static::lazy_static! {
     static ref JIEBA: Jieba = jieba_rs::Jieba::new();
@@ -25,6 +24,46 @@ pub fn token_stream(text: &str) -> BoxTokenStream {
             position: token_idx + 1,
             position_length: origin_tokens_len,
         });
+
+        #[cfg(feature = "pinyin")]
+        {
+            // every token
+            use pinyin::ToPinyin;
+            let pinyin: String = token
+                .word
+                .to_pinyin()
+                .flatten()
+                .map(|p| p.plain())
+                .collect();
+
+            tokens.push(Token {
+                offset_from: indices[token.start].0,
+                offset_to: indices[token.end].0,
+                text: pinyin,
+                position: token_idx + 1,
+                position_length: origin_tokens_len,
+            });
+        }
+    }
+
+    #[cfg(feature = "pinyin")]
+    {
+        // every Chinese char
+        use pinyin::ToPinyin;
+        let text_len = text.chars().count();
+        text.chars()
+            .filter_map(|c| c.to_pinyin())
+            .map(|p| p.plain())
+            .enumerate()
+            .for_each(|(i, p)| {
+                tokens.push(Token {
+                    offset_from: indices[i].0,
+                    offset_to: indices[i + 1].0,
+                    text: String::from(p),
+                    position: i + 1,
+                    position_length: text_len,
+                });
+            });
     }
 
     BoxTokenStream::from(MetaTokenStream { tokens, index: 0 })
