9a6a4e0cbcbfdf924fe1f11289551b309d8b46d0
zu1k <i@zu1k.com>
2023-01-18 19:39:10 +0800

    feat: frontend use system as initial color mode

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
+  initialColorMode: 'system',
   useSystemColorMode: true
 };
 
