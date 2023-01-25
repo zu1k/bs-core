a04472ffdc016c60c55f5ff299b4d56d36e55225
zu1k <i@zu1k.com>
2023-01-25 09:58:01 +0800

    feat(frontend): add PWA support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import faviconsPlugin from '@darkobits/vite-plugin-favicons';
 import react from '@vitejs/plugin-react';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -19,7 +20,57 @@ export default defineConfig(() => {
         ? faviconsPlugin({
             icons: { favicons: { source: '../crates/book-searcher-desktop/icons/icon.png' } }
           })
-        : null
+        : null,
+      VitePWA({
+        injectRegister: 'auto',
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Book Searcher',
+          short_name: 'Book Searcher',
+          description: 'Easy and fast book searcher, create and search your private library.',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'icon.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: 'icon.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            }
+          ]
+        },
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true,
+          runtimeCaching: [
+            {
+              urlPattern: /search/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'search-cache'
+              }
+            },
+            {
+              urlPattern: /(.*?)\.(js|css)/,
+              handler: 'StaleWhileRevalidate',
+              options: {
+                cacheName: 'js-css-cache'
+              }
+            },
+            {
+              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|ico|bmp|psd|tiff|tga|eps)/,
+              handler: 'StaleWhileRevalidate',
+              options: {
+                cacheName: 'image-cache'
+              }
+            }
+          ]
+        }
+      })
     ],
     build: {
       rollupOptions: {
