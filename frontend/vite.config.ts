444dc644cb0113a6a17cce390e14e85d699175c4
zu1k <i@zu1k.com>
2023-01-28 12:00:33 +0800

    Bump version to 0.9.1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,61 @@
 import { defineConfig } from 'vite';
-import faviconsPlugin from '@darkobits/vite-plugin-favicons';
+import { faviconsPlugin } from '@darkobits/vite-plugin-favicons';
 import react from '@vitejs/plugin-react';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { VitePWA } from 'vite-plugin-pwa';
+
+const websiteManifest = {
+  name: 'Book Searcher',
+  short_name: 'Book Searcher',
+  description: 'Easy and fast book searcher, create and search your private library.',
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: 'icon.png',
+      sizes: '512x512',
+      type: 'image/png'
+    },
+    {
+      src: 'icon.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable'
+    }
+  ]
+};
+
+const vitePWAPlugin = VitePWA({
+  injectRegister: 'auto',
+  registerType: 'autoUpdate',
+  manifest: websiteManifest,
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /search/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'search-cache'
+        }
+      },
+      {
+        urlPattern: /(.*?)\.(js|css)/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'js-css-cache'
+        }
+      },
+      {
+        urlPattern: /(.*?)\.(png|jpe?g|svg|gif|ico|bmp|psd|tiff|tga|eps)/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'image-cache'
+        }
+      }
+    ]
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -18,59 +71,20 @@ export default defineConfig(() => {
       react(),
       process.env.VITE_TAURI === '0'
         ? faviconsPlugin({
-            icons: { favicons: { source: '../crates/book-searcher-desktop/icons/icon.png' } }
+            icons: {
+              favicons: {
+                source: '../crates/book-searcher-desktop/icons/icon.png'
+              },
+              android: {
+                source: '../crates/book-searcher-desktop/icons/icon.png'
+              },
+              appleStartup: {
+                source: '../crates/book-searcher-desktop/icons/icon.png'
+              }
+            }
           })
         : null,
-      VitePWA({
-        injectRegister: 'auto',
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Book Searcher',
-          short_name: 'Book Searcher',
-          description: 'Easy and fast book searcher, create and search your private library.',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'icon.png',
-              sizes: '512x512',
-              type: 'image/png'
-            },
-            {
-              src: 'icon.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable'
-            }
-          ]
-        },
-        workbox: {
-          clientsClaim: true,
-          skipWaiting: true,
-          runtimeCaching: [
-            {
-              urlPattern: /search/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'search-cache'
-              }
-            },
-            {
-              urlPattern: /(.*?)\.(js|css)/,
-              handler: 'StaleWhileRevalidate',
-              options: {
-                cacheName: 'js-css-cache'
-              }
-            },
-            {
-              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|ico|bmp|psd|tiff|tga|eps)/,
-              handler: 'StaleWhileRevalidate',
-              options: {
-                cacheName: 'image-cache'
-              }
-            }
-          ]
-        }
-      })
+      vitePWAPlugin
     ],
     build: {
       rollupOptions: {
