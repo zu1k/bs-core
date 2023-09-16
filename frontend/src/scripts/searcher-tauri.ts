9d035e2e4c1e2ecf5695214268f8a07dba904feb
忘忧北萱草 <wybxc@qq.com>
2023-09-16 22:40:55 +0800

    fix(frontend): tauri search api

--- frontend/src/scripts/searcher-tauri.ts
+++ frontend/src/scripts/searcher-tauri.ts
@@ -2,8 +2,12 @@ import type { Book, SearchQuery } from './searcher';
 import { invoke } from '@tauri-apps/api';
 
 export default async function search(query: SearchQuery) {
-  const response = await invoke('search', { query, limit: query.limit, offset: query.offset });
-  const books = response as Book[] | undefined;
-  const total = 0;
+  const response = (await invoke('search', {
+    query,
+    limit: query.limit,
+    offset: query.offset
+  })) as [Book[] | undefined, number];
+  const books = response[0];
+  const total = response[1];
   return { books, total };
 }
