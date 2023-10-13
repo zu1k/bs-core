1f71f81262368e566f38990a085811b0b5922f87
zu1k <i@zu1k.com>
2023-10-13 10:43:55 +0800

    feat(frontend): more paging button

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,16 +1,14 @@
 import {
   Flex,
-  IconButton,
-  Icon,
   IconButtonProps,
-  Text,
   FlexProps,
   NumberInput,
-  NumberInputField
+  NumberInputField,
+  Button
 } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { TbChevronsLeft, TbChevronLeft, TbChevronRight, TbChevronsRight } from 'react-icons/tb';
+import { useMediaQuery } from 'react-responsive';
 
 export interface PaginationProps extends FlexProps {
   pageCount: number;
@@ -37,60 +35,56 @@ export default function Pagination({
 }: PaginationProps) {
   const { t } = useTranslation();
   const jumpToPageInput = useRef<HTMLInputElement>(null);
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 700px)' });
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1200px)' });
+  const pageButtonSidecount = isBigScreen ? 3 : isSmallScreen ? 1 : 2;
 
   if (pageCount === 0) return <Flex {...props} gap={1} justify="flex-end" wrap="wrap" />;
 
+  function genPageButtonList(pageIdx: number): number[] {
+    const start = pageIdx > pageButtonSidecount ? pageIdx - pageButtonSidecount : 0;
+    const end =
+      pageIdx < pageCount - pageButtonSidecount ? pageIdx + pageButtonSidecount : pageCount - 1;
+    const sequence: number[] = [...Array.from({ length: end - start + 1 }, (_, i) => i + start)];
+    return sequence;
+  }
+
   return (
     <Flex {...props} gap={1} justify="flex-end" wrap="wrap">
-      <IconButton
-        aria-label={t('table.first_page')}
+      <Button
+        display={pageIndex < pageButtonSidecount + 1 ? 'none' : 'inline-flex'}
         title={t('table.first_page') ?? ''}
-        icon={<Text>{1}</Text>}
-        display={{ base: 'none', md: 'inline-flex' }}
         onClick={() => setPageIndex(0)}
         disabled={!canPreviousPage}
-      />
-
-      <IconButton
-        aria-label={t('table.previous_page')}
-        title={t('table.previous_page') ?? ''}
-        icon={<Icon as={TbChevronLeft} />}
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}
-      />
-      {[pageIndex - 2, pageIndex - 1, pageIndex, pageIndex + 1, pageIndex + 2]
-        .filter((i) => i >= 0 && i < pageCount)
-        .map((idx) => {
-          const title = t('table.page', { page: idx + 1 });
-          const disabled = pageIndex === idx;
-          const style: Partial<IconButtonProps> = disabled ? { colorScheme: 'blue' } : {};
-          return (
-            <IconButton
-              aria-label={title}
-              title={title}
-              key={idx}
-              icon={<Text>{idx + 1}</Text>}
-              onClick={() => setPageIndex(idx)}
-              disabled={disabled}
-              {...style}
-            />
-          );
-        })}
-      <IconButton
-        aria-label={t('table.next_page')}
-        title={t('table.next_page') ?? ''}
-        icon={<Icon as={TbChevronRight} />}
-        onClick={() => nextPage()}
-        disabled={!canNextPage}
-      />
-      <IconButton
-        aria-label={t('table.last_page')}
+      >
+        {1}
+      </Button>
+      {genPageButtonList(pageIndex).map((idx) => {
+        const title = t('table.page', { page: idx + 1 });
+        const disabled = pageIndex === idx;
+        const style: Partial<IconButtonProps> = disabled ? { colorScheme: 'blue' } : {};
+        return (
+          <Button
+            aria-label={title}
+            title={title}
+            key={idx}
+            onClick={() => setPageIndex(idx)}
+            disabled={disabled}
+            spacing="0"
+            {...style}
+          >
+            {idx + 1}
+          </Button>
+        );
+      })}
+      <Button
+        display={pageIndex > pageCount - pageButtonSidecount - 2 ? 'none' : 'inline-flex'}
         title={t('table.last_page') ?? ''}
-        icon={<Text>{pageCount}</Text>}
-        display={{ base: 'none', md: 'inline-flex' }}
         onClick={() => setPageIndex(pageCount - 1)}
         disabled={!canNextPage}
-      />
+      >
+        {pageCount}
+      </Button>
       <NumberInput
         defaultValue={pageIndex + 1}
         key={`${pageIndex + 1}`}
